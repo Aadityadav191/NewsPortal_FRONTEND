cd20a57f8400503721ddbe036a250c9cce7a2e96
Aadit yadav <yaaditya191@gmail.com>
2026-08-05 14:08:39 +0545

    feat: implement cookie consent popup

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,37 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext';
-import AppRoutes from './routes/AppRoutes';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import AppRoutes from "./routes/AppRoutes";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import CookieConsent from "./components/common/CookieConcent";
 
 function App() {
+  const [showPopup, setShowPopup] = useState(false);
+  const [showConsent, setShowConsent] = useState(false);
+
+  useEffect(() => {
+    const consent = localStorage.getItem("cookie_consent");
+    if (!consent) {
+      setTimeout(() => setShowConsent(true), 10000);
+    }
+  }, []);
+
+  const handleAccept = () => {
+    localStorage.setItem("cookie_consent", "true");
+    setShowConsent(false);
+  };
   return (
-    <BrowserRouter>
-      <AuthProvider>
-        <AppRoutes />
-        <ToastContainer />
-      </AuthProvider>
-    </BrowserRouter>
+    <>
+      {showPopup && <Popup onClose={() => setShowPopup(false)} />}
+      {showConsent && <CookieConsent onAccept={handleAccept} />}
+      <BrowserRouter>
+        <AuthProvider>
+          <AppRoutes />
+          <ToastContainer />
+        </AuthProvider>
+      </BrowserRouter>
+    </>
   );
 }
 
