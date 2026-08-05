0ff10815d6874ebff6f3bb86dce337fc06685afd
Aadit yadav <yaaditya191@gmail.com>
2026-08-05 15:23:17 +0545

    chore: add .env.example and update gitignore rules

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import AppRoutes from "./routes/AppRoutes";
@@ -7,24 +7,8 @@ import "react-toastify/dist/ReactToastify.css";
 import CookieConsent from "./components/common/CookieConcent";
 
 function App() {
-  const [showPopup, setShowPopup] = useState(false);
-  const [showConsent, setShowConsent] = useState(false);
-
-  useEffect(() => {
-    const consent = localStorage.getItem("cookie_consent");
-    if (!consent) {
-      setTimeout(() => setShowConsent(true), 10000);
-    }
-  }, []);
-
-  const handleAccept = () => {
-    localStorage.setItem("cookie_consent", "true");
-    setShowConsent(false);
-  };
   return (
     <>
-      {showPopup && <Popup onClose={() => setShowPopup(false)} />}
-      {showConsent && <CookieConsent onAccept={handleAccept} />}
       <BrowserRouter>
         <AuthProvider>
           <AppRoutes />
